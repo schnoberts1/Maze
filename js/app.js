37dc0f80797393c6757309136dc5c53323237307
Andy Schneider <aschneider@getcollc.com>
2015-01-23 14:54:45 +0000

    Single ball and moving with keys

--- js/app.js
+++ js/app.js
@@ -1,25 +1,13 @@
 App = function (spec) {
-    that = {maze: undefined, ball: undefined};
+    var self = {maze: undefined, ball: undefined};
 
-    that.bounce = function () {
-        this.ball.move(this.ball.x + 1, this.ball.y + 1);
-    };
-
-    that.onReady = function () {
-        this.maze = Maze({target: '#gameCanvas'});
-        this.ball = this.maze.newBall();
-        this.ball.draw();
-
-        var This = this;
-
-        setInterval(function () {
-            This.bounce();
-        }, 500);
+    self.onReady = function () {
+        self.maze = Maze({target: '#gameCanvas'});
     };
 
     $(document).ready(function () {
-        that.onReady();
+        self.onReady();
     });
 
-    return that;
+    return self;
 };
--- js/maze.js
+++ js/maze.js
@@ -1,50 +1,77 @@
-Ball = function(spec)
-{
-    var that = {x: spec.x, y: spec.y, radius: 32, canvas: spec.canvas, oldx: undefined, oldy: undefined};
+Ball = function (spec) {
+    var self = {x: spec.x, y: spec.y, radius: 32, canvas: spec.canvas, step: 4, oldx: undefined, oldy: undefined};
 
-    that.draw = function () {
+    self.draw = function () {
         var canvas = this.canvas;
         var context = canvas.getContext('2d');
 
         context.beginPath();
-        if (this.oldx) {
-            context.clearRect(this.oldx - this.radius, this.oldy - this.radius, this.radius * 2, this.radius * 2);
+        if (self.oldx) {
+            context.clearRect(self.oldx - self.radius - 1, self.oldy - self.radius - 1, self.radius * 2 + 2, self.radius * 2 + 2);
         }
         context.fillStyle = '#0000ff';
 
         // x,y,radius,startAngle,endAngle,anticlockwise
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.arc(self.x, self.y, self.radius, 0, Math.PI * 2);
         context.closePath();
         context.fill();
     };
 
-    that.move = function (x, y) {
-        this.oldx = this.x;
-        this.oldy = this.y;
+    self.move = function (x, y) {
+        self.oldx = self.x;
+        self.oldy = self.y;
 
-        this.x = x;
-        this.y = y;
+        self.x = x;
+        self.y = y;
 
-        console.log("Move -> " + this.x + "," + this.y);
-
-        this.draw();
+        self.draw();
     };
 
-    return that;
+    self.left = function () {
+        self.move(self.x - self.step, self.y);
+    };
+
+    self.right = function () {
+        self.move(self.x + self.step, self.y);
+    };
+
+    self.up = function () {
+        self.move(self.x, self.y - self.step);
+    };
+
+    self.down = function () {
+        self.move(self.x, self.y + self.step);
+    };
+
+    return self;
 };
 
 
-Maze = function(spec)
-{
-    var that = {};
+Maze = function (spec) {
+    var self = {canvas: $(spec.target)[0], keyResponders: {}, ball: undefined};
 
-    that.canvas = $(spec.target)[0];
-
-    that.newBall = function () {
+    self.newBall = function () {
         return Ball({x: 100, y: 100, canvas: this.canvas})
     };
 
-    return that;
+    self.keyDown = function (e) {
+        var handler = this.keyResponders[e.keyCode];
+
+        if (handler) {
+            handler();
+        }
+    };
+
+    self.ball = self.newBall();
+    self.ball.draw();
+
+    self.keyResponders = {37: self.ball.left, 38: self.ball.up, 39: self.ball.right, 40: self.ball.down};
+
+    $("body").keydown(function (e) {
+        self.keyDown(e);
+    });
+
+    return self;
 };
 
 
