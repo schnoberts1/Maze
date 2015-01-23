c509084b7646e14793c756bc295d70a2e28ba751
Andy Schneider <aschneider@getcollc.com>
2015-01-23 11:48:47 +0000

    Single ball

--- js/maze.js
+++ js/maze.js
@@ -1,20 +1,36 @@
-
 Ball = function(spec)
 {
-    var that = { x: 0, y: 0 }
+    var that = {x: spec.x, y: spec.y, canvas: spec.canvas};
 
-    that
+    that.draw = function () {
+        var canvas = this.canvas;
+        var context = canvas.getContext('2d');
+
+        context.beginPath();
+        context.fillStyle = '#0000ff';
+
+        // x,y,radius,startAngle,endAngle,anticlockwise
+        context.arc(this.x, this.y, 32, 0, Math.PI * 2);
+        context.closePath();
+        context.fill();
+    };
 
     return that;
-}
+};
 
 
 Maze = function(spec)
 {
     var that = {};
 
+    that.canvas = $(spec.target)[0];
+
+    that.newBall = function () {
+        return Ball({x: 100, y: 100, canvas: this.canvas})
+    };
+
     return that;
-}
+};
 
 
 
