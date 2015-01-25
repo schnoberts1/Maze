d7fd26b8d2854344f3045298a54406656b8583be
James Fryer <git@schnoberts.com>
2015-01-25 09:40:22 +0000

    Work in progress

--- js/maze.js
+++ js/maze.js
@@ -1,65 +1,11 @@
 ////////////////////////////////
 
-/**
- * A ball on the screen.
- */
-Ball = function (spec) {
-    var self = {x: spec.x, y: spec.y, radius: 32, canvas: spec.canvas, step: 4, oldx: undefined, oldy: undefined};
-
-    self.draw = function () {
-        var canvas = this.canvas;
-        var context = canvas.getContext('2d');
-
-        context.beginPath();
-        if (self.oldx) {
-            context.clearRect(self.oldx - self.radius - 1, self.oldy - self.radius - 1, self.radius * 2 + 2, self.radius * 2 + 2);
-        }
-        context.fillStyle = '#0000ff';
-
-        // x,y,radius,startAngle,endAngle,anticlockwise
-        context.arc(self.x, self.y, self.radius, 0, Math.PI * 2);
-        context.closePath();
-        context.fill();
-    };
-
-    self.move = function (x, y) {
-        self.oldx = self.x;
-        self.oldy = self.y;
-
-        self.x = x;
-        self.y = y;
-
-        self.draw();
-    };
-
-    self.left = function () {
-        self.move(self.x - self.step, self.y);
-    };
-
-    self.right = function () {
-        self.move(self.x + self.step, self.y);
-    };
-
-    self.up = function () {
-        self.move(self.x, self.y - self.step);
-    };
-
-    self.down = function () {
-        self.move(self.x, self.y + self.step);
-    };
-
-    return self;
-};
 
 /**
- * A maze with one ball and no walls!
+ * A maze with one pacman and no walls!
  */
 Maze = function (spec) {
-    var self = {canvas: $(spec.target)[0], keyResponders: {}, ball: undefined};
-
-    self.newBall = function () {
-        return Ball({x: 100, y: 100, canvas: this.canvas})
-    };
+    var self = {canvas: $(spec.target)[0], keyResponders: {}, pacman: undefined};
 
     self.keyDown = function (e) {
         var handler = this.keyResponders[e.keyCode];
@@ -71,10 +17,10 @@ Maze = function (spec) {
         e.preventDefault();
     };
 
-    self.ball = self.newBall();
-    self.ball.draw();
+    self.pacman = Pacman({x: 100, y: 100, canvas: self.canvas});
+    self.pacman.draw();
 
-    self.keyResponders = {37: self.ball.left, 38: self.ball.up, 39: self.ball.right, 40: self.ball.down};
+    self.keyResponders = {37: self.pacman.left, 38: self.pacman.up, 39: self.pacman.right, 40: self.pacman.down};
 
     $("body").keydown(function (e) {
         self.keyDown(e);
---
+++ js/pacman.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Fryer on 24/01/2015.
+ */
+
+/**
+ * A pacman on the screen.
+ */
+Pacman = function (spec) {
+    var self = {x: spec.x,
+                y: spec.y,
+                radius: 32,
+                canvas: spec.canvas,
+                step: 4,
+                oldx: undefined,
+                oldy: undefined,
+                pacmanClosed:true };
+
+    self.draw = function () {
+        var canvas = self.canvas;
+        var context = canvas.getContext('2d');
+
+        context.beginPath();
+        if (self.oldx) {
+            context.clearRect(self.oldx - self.radius - 1, self.oldy - self.radius - 1, self.radius * 2 + 2, self.radius * 2 + 2);
+        }
+        context.fillStyle = '#0000ff';
+        if(self.pacmanClosed)
+        {
+            context.arc(self.x, self.y, self.radius,0,2*Math.PI)
+        }
+        else
+        {
+            context.arc(self.x, self.y, self.radius,(Math.PI / 180) * 40, (Math.PI / 180) * 320);
+            context.lineTo(self.x, self.y)
+        }
+
+        context.fill();
+        context.closePath();
+
+    };
+
+    self.move = function (x, y) {
+        self.pacmanClosed = !self.pacmanClosed
+
+        self.oldx = self.x;
+        self.oldy = self.y;
+
+        self.x = x;
+        self.y = y;
+
+        self.draw();
+    };
+
+    self.left = function () {
+        self.move(self.x - self.step, self.y);
+    };
+
+    self.right = function () {
+        self.move(self.x + self.step, self.y);
+    };
+
+    self.up = function () {
+        self.move(self.x, self.y - self.step);
+    };
+
+    self.down = function () {
+        self.move(self.x, self.y + self.step);
+    };
+
+    return self;
+};
