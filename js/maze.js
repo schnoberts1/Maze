f8682e9f32f74d93332597dffd3e98c0daf7c72d
James Fryer <jamesfryer1998@outlook.com>
2015-01-25 11:09:39 +0000

    Log tilt

--- js/maze.js
+++ js/maze.js
@@ -7,14 +7,14 @@
 Maze = function (spec) {
     var self = {canvas: $(spec.target)[0], keyResponders: {}, pacman: undefined};
 
-    self.keyDown = function (e) {
-        var handler = this.keyResponders[e.keyCode];
+    self.keyDown = function (event) {
+        var handler = this.keyResponders[event.keyCode];
 
         if (handler) {
             handler();
         }
 
-        e.preventDefault();
+        event.preventDefault();
     };
 
     self.pacman = Pacman({x: 100, y: 100, canvas: self.canvas});
@@ -22,9 +22,28 @@ Maze = function (spec) {
 
     self.keyResponders = {37: self.pacman.left, 38: self.pacman.up, 39: self.pacman.right, 40: self.pacman.down};
 
-    $("body").keydown(function (e) {
-        self.keyDown(e);
-    });
+    self.onDeviceOrientation=function (event) {
+        if (event.beta && event.gamma) {
+            $("#log").text("Beta: " + event.beta + " Gamma: " + event.gamma);
+        }
+        else
+        {
+            $("#log").text("Garbage");
+        }
+    }
+
+    if(window.DeviceOrientationEvent){
+        $("#log").text("Tilt mode");
+        window.addEventListener("deviceorientation",self.onDeviceOrientation)
+    }
+    else
+    {
+        $("body").keydown(function (e) {
+            $("#log").text("Keyboard mode");
+            self.keyDown(e);
+
+        });
+    }
 
     return self;
 };
